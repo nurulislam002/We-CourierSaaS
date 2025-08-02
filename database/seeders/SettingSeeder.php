1038808f2be7f1cfc787d23addcf66e3e0712d73
riadolivine <devolivineriad@gmail.com>
2025-08-02 16:00:55 +0600

    fixed issue seeders

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -17,5 +17,75 @@ class SettingSeeder extends Seeder
     {
 
 
+        //social login settings
+        //facebook
+        Setting::create(['company_id'=>1,'key' => 'facebook_client_id',     'value' => '']);
+        Setting::create(['company_id'=>1,'key' => 'facebook_client_secret', 'value' => '']);
+        Setting::create(['company_id'=>1,'key' => 'facebook_status',        'value' => 1]);
+        //google
+        Setting::create(['company_id'=>1,'key' => 'google_client_id',     'value' => '']);
+        Setting::create(['company_id'=>1,'key' => 'google_client_secret', 'value' => '']);
+        Setting::create(['company_id'=>1,'key' => 'google_status',        'value' => 1]);
+
+
+        $this->supperAdminPaymentGateway();
+
+        //===== payment setup ===
+        //stripe
+        Setting::create(['company_id'=>2,'key' => 'stripe_publishable_key',     'value' => '']);
+        Setting::create(['company_id'=>2,'key' => 'stripe_secret_key',          'value' => '']);
+        Setting::create(['company_id'=>2,'key' => 'stripe_status',              'value' => 1]);
+
+        //Razorpay
+        Setting::create(['company_id'=>2,'key' => 'razorpay_key',               'value' => '']);
+        Setting::create(['company_id'=>2,'key' => 'razorpay_secret',            'value' => '']);
+        Setting::create(['company_id'=>2,'key' => 'razorpay_status',            'value' => 1]);
+
+        //sslcommerz
+        Setting::create(['company_id'=>2,'key' => 'sslcommerz_store_id',        'value' => '']);
+        Setting::create(['company_id'=>2,'key' => 'sslcommerz_store_password',  'value' => '']);
+        Setting::create(['company_id'=>2,'key' => 'sslcommerz_testmode',        'value' => 1]);
+        Setting::create(['company_id'=>2,'key' => 'sslcommerz_status',          'value' => 1]);
+
+        //paypal
+        Setting::create(['company_id'=>2,'key' => 'paypal_client_id',              'value' => '']);
+        Setting::create(['company_id'=>2,'key' => 'paypal_client_secret',          'value' => '']);
+        Setting::create(['company_id'=>2,'key' => 'paypal_mode',                   'value' => 'sendbox']);
+        Setting::create(['company_id'=>2,'key' => 'paypal_status',                 'value' => 1]);
+
+        //skrill
+        Setting::create(['company_id'=>2,'key' => 'skrill_merchant_email',         'value' => 'demoqco@sun-fish.com']);
+        Setting::create(['company_id'=>2,'key' => 'skrill_status',                 'value' => 1]);
+
+
+        // //bkash
+        Setting::create(['company_id'=>2,'key' => 'bkash_app_id',              'value' => 'application id']);
+        Setting::create(['company_id'=>2,'key' => 'bkash_app_secret',          'value' => 'application secret key']);
+        Setting::create(['company_id'=>2,'key' => 'bkash_username',            'value' => 'username']);
+        Setting::create(['company_id'=>2,'key' => 'bkash_password',            'value' => 'password']);
+        Setting::create(['company_id'=>2,'key' => 'bkash_test_mode',           'value' => 1]);
+        Setting::create(['company_id'=>2,'key' => 'bkash_status',              'value' => 1]);
+
+
+        //aamar pay
+        Setting::create(['company_id'=>2,'key' => 'aamarpay_store_id',        'value' => 'aamarypay']);
+        Setting::create(['company_id'=>2,'key' => 'aamarpay_signature_key',   'value' => '']);
+        Setting::create(['company_id'=>2,'key' => 'aamarpay_sendbox_mode',    'value' => 1]);
+        Setting::create(['company_id'=>2,'key' => 'aamarpay_status',          'value' => 1]);
+          
+        //=====payment setup===
+ 
+
     }
+  
+    public function supperAdminPaymentGateway(){
+        //===== payment setup ===
+        //stripe
+        Setting::create(['company_id'=>1,'key' => 'stripe_publishable_key',     'value' => '']);
+        Setting::create(['company_id'=>1,'key' => 'stripe_secret_key',          'value' => '']);
+        Setting::create(['company_id'=>1,'key' => 'stripe_status',              'value' => 1]);
+ 
+ 
+    }
+
 }
